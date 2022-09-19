85f189dd9791c5c5dd2f1422a93748dee2b16f0e
Jonathan Conger <44207939+jonathanconger@users.noreply.github.com>
2022-09-19 17:07:31 -0600

    observer fix

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,14 +1,37 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Goal : MonoBehaviour
 {
-	[SerializeField] WinObserver winObserver;
+	UnityEvent eventLevelEnd;
+	[SerializeField] GameObject winOverlay;
+	[SerializeField] GameObject playerObject;
+
+	private void Awake()
+	{
+		if (eventLevelEnd == null)
+			eventLevelEnd = new UnityEvent();
+
+		eventLevelEnd.AddListener(DisablePlayerInput);
+		eventLevelEnd.AddListener(DisplayWinOverlay);
+	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.gameObject.CompareTag("Player"))
 		{
-			winObserver.OnLevelWin.Invoke();
+			eventLevelEnd.Invoke();
 		}
+	}
+
+	private void DisablePlayerInput()
+	{
+		playerObject.GetComponent<PlayerController>().enabled = false;
+		playerObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+	}
+
+	private void DisplayWinOverlay()
+	{
+		winOverlay.SetActive(true);
 	}
 }
